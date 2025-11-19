55be39b9b0588b1f0bfe6fe0a5acbfae40d446f7
HLBoun <154765503+HLBoun@users.noreply.github.com>
2025-11-19 10:34:08 -0600

    Fixed Code. Mostly out of place syntax (eg parenth. braces etc)

--- Main (1).kt
+++ Main (1).kt
@@ -1,32 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main()
+fun main() 
 {
     println("Hello poor soul, what is your name?\n")
     val name = readln()
+
     println("Welcome to your judgment," + name + "!\n")
     println(
-        "Choosing a door will determine the fate for your eternal soul.\n" +
-                "Enter a number [1], [2], or [3]...\n")
-}
-{
+        "Choosing a door will determine the fate for your eternal soul.\n"
+                + "Enter a number [1], [2], or [3]...\n")
+
+    val userChoice = readln()
     println("You have chosen door:" + userChoice)
+
     if (userChoice == "1")
-}
-{
+    {
         println("This door revealed a hungry lion that shall chew on yur bones for the rest of eternity")
+    }
 
-}
-
-{
-    if (userChoice == "2")
-        println("This door reveals a pit of deadly snakes and you have fallen into the pit."
-}
-{
-    if (userChoice == "3")
+    if (userChoice == "2") 
+    {
+        println("This door reveals a pit of deadly snakes and you have fallen into the pit.")
+    }
+    if (userChoice == "3") 
+    {
         println("This door reveals everything you ever wanted, money, girls anything.")
-}
-{
-        val userChoice = readln()
 
+        val userChoice = readln()
+    }
 }
